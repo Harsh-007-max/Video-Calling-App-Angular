1366db4f1570f0dd08f44367f8b3d48b3f3eff0d
harsh <harshbhalodiya1209@gmail.com>
2024-05-29 00:25:57 +0530

    FIX: remote audio not comming

--- src/app/environments/environment.default.ts
+++ src/app/environments/environment.default.ts
@@ -8,5 +8,5 @@ export const defaultEnvironment = {
     appId: '1:468571200261:web:6dc7237f33b16b7999756c',
   },
   production: false,
-  socketAPI: 'https://angular-video-calling-app-server.onrender.com',
+  socketAPI: 'http://localhost:8000',
 };
